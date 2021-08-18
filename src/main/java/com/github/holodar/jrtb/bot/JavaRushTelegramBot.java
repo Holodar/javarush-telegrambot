a84a2806d4ced13a7c9f7f94f05740dd3451ce3b
Tornike <tornike1925@gmail.com>
2021-08-18 13:51:51 +0300

    added commant telegram bot

--- src/main/java/com/github/holodar/jrtb/bot/JavaRushTelegramBot.java
+++ src/main/java/com/github/holodar/jrtb/bot/JavaRushTelegramBot.java
@@ -2,14 +2,19 @@ package com.github.holodar.jrtb.bot;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.springframework.beans.factory.annotation.Value;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import com.github.holodar.jrtb.command.CommandContainer;
+import com.github.holodar.jrtb.service.SendBotMessageServiceImpl;
 
+import java.util.Locale;
+
+import static com.github.holodar.jrtb.command.CommandName.NO;
 
 @Component
 public class JavaRushTelegramBot extends TelegramLongPollingBot{
+
+    public static String COMMAND_PREFIX = "/";
 
     @Value("${bot.username}")
     private String username;
@@ -17,21 +22,23 @@ public class JavaRushTelegramBot extends TelegramLongPollingBot{
     @Value("${bot.token}")
     private String token;
 
+    private final CommandContainer commandContainer;
+
+    public JavaRushTelegramBot(){
+        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this));
+    }
+
 
     @Override
     public void onUpdateReceived(Update update){
         if(update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText().trim();
-            String chatId = update.getMessage().getChatId().toString();
+            if(message.startsWith(COMMAND_PREFIX)){
+                String commandIdentifier = message.split(" ")[0].toLowerCase();
 
-            SendMessage sm = new SendMessage();
-            sm.setChatId(chatId);
-            sm.setText(message);
-
-            try {
-                execute(sm);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+                commandContainer.retriveCommand(commandIdentifier).execute(update);
+            }else {
+                commandContainer.retriveCommand(NO.getCommandName()).execute(update);
             }
         }
     }
