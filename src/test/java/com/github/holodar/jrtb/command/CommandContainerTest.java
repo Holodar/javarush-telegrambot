50568ea6c783d26a9e3519652c6beb7b743d82ed
Tornike <tornike1925@gmail.com>
2021-08-18 21:00:04 +0300

    JRTB-3: updated RELEASE_NOTES.md

---
+++ src/test/java/com/github/holodar/jrtb/command/CommandContainerTest.java
@@ -0,0 +1,38 @@
+package com.github.holodar.jrtb.command;
+
+import com.github.holodar.jrtb.service.SendBotMessageService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+
+@DisplayName("Unit-level testing for CommandContainer")
+class CommandContainerTest {
+
+    private CommandContainer commandContainer;
+
+    @BeforeEach
+    public void imit(){
+        SendBotMessageService sendBotMessageService = Mockito.mock(SendBotMessageService.class);
+        commandContainer = new CommandContainer(sendBotMessageService);
+    }
+    @Test
+    public void shouldGetAllTheExistingCommands(){
+        Arrays.stream(CommandName.values()).forEach(commandName -> {
+            Command command = commandContainer.retriveCommand(commandName.getCommandName());
+            Assertions.assertNotEquals(UnknownCommand.class,command.getClass());
+        });
+    }
+    @Test
+    public void shouldReturnUnknownCommand() {
+        String unknownCommand = "/fgjhdfgdfg";
+
+        Command command = commandContainer.retriveCommand(unknownCommand);
+
+        Assertions.assertNotEquals(UnknownCommand.class,command.getClass());
+    }
+
+}
---
+++ src/test/java/com/github/holodar/jrtb/command/NoCommandTest.java
@@ -0,0 +1,23 @@
+package com.github.holodar.jrtb.command;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static com.github.holodar.jrtb.command.CommandName.NO;
+import static com.github.holodar.jrtb.command.NoCommand.NO_MESSAGE;
+
+@DisplayName("Unit-level testing for NpCommand")
+public class NoCommandTest extends  AbstractCommandTest{
+
+    @Override
+    String getCommandName(){
+        return NO.getCommandName();
+    }
+    @Override
+    String getCommandMessage(){
+        return NO_MESSAGE;
+    }
+    @Override
+    Command getCommand(){
+        return new NoCommand(sendBotMessageService);
+    }
+}
